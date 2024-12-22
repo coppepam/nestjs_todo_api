7c7e6af1efa41d07f3a81587ddf3af081bdc2a34
coppe <coppepam@gmail.com>
2024-12-22 12:36:39 +0900

    WIP csrf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-// import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
-// import * as csurf from 'csurf';
+import csurf from 'csurf';
+import { Request } from 'express';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +14,18 @@ async function bootstrap() {
     credentials: true, // クライアント側でCookieを使うための設定
   });
   app.use(cookieParser()); // Cookie のパース
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: process.env.NODE_ENV !== 'development',
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
